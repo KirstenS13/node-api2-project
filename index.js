a6b0af69652097b0d65f8e358b9dd50fba2f5c4d
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-09 20:05:05 -0400

    set up the skeleton of posts-router, imported it to index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // 1 - bring in express
 const express = require('express');
 // 1.5 - bring in routers
-
+const postsRouter = require('./posts/posts-router');
 
 // 2 - create server
 const server = express();
@@ -12,7 +12,12 @@ const server = express();
 // 3 - add middleware to parse JSON request bodies
 server.use(express.json());
 // 3.5 - add the routers
+server.use(postsRouter);
 
+// Optional - define endpoints that don't live in routers
+server.get("/", (req, res) => {
+    res.json({ message: "Hi, it's me the server. I'm working." });
+});
 
 // 4 - have the server start listening
 server.listen(8000, () => {
---
+++ posts/posts-router.js
@@ -0,0 +1,15 @@
+// router for the posts goes here
+
+// make a router
+// 1 - bring in express
+const express = require('express');
+// 1.5 - bring in database file
+const db = require('../data/db');
+
+// 2 - create the router with express
+const router = express.Router();
+
+// 3 - define endpoints
+
+// 4 - export the whole router
+module.exports = router;
